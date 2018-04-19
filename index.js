d461842faec1e57af6c7cdf5047b7633eb2a6779
Kerry J J <nuscience43@outlook.com>
2018-04-19 00:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
   return("I can\'t hear you!");
   }
     if(string.toUpperCase() === "HELLO"){
-   return("YES INDEED!");
+   return(`YES INDEED!`);
     }
   if ("I love you, Grandma.");{
   return("I love you, too.");
