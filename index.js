3f9b5f6338593f6ac7ef1e10dbecf437a3051990
Kerry J J <nuscience43@outlook.com>
2018-04-19 00:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if("hello"){
+  if(string.toLowerCase() === "hello"){
   return("I can\'t hear you!");
   }
    else if("HELLO"){
